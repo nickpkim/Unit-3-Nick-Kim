b10670bf44b37949a212af7fe909c10f28de302e
Nick Kim <kimn@wlhs.wlwv.k12.or.us>
2019-10-03 09:39:17 -0700

    Completed drinking age

--- DrinkingAge.java
+++ DrinkingAge.java
@@ -3,6 +3,11 @@ public class DrinkingAge{
   public static void main(String[] args){
     Scanner scan = new Scanner(System.in);
     int age = scan.nextInt();
-    //if (age>=17 && age<21);
+    if (age>=17 && age<21)
+      System.out.println("can drive, no drinking");
+    else if (age>=21)
+      System.out.println("can drive, drink after");
+    else
+      System.out.println("no driving nor drinking");
   }
 }
