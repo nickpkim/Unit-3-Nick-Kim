6885d3108760ba5e020a64c68a405c7f96476179
Nick Kim <kimn@wlhs.wlwv.k12.or.us>
2019-10-04 09:14:46 -0700

    updated win percentage

--- WinPercentage.java
+++ WinPercentage.java
@@ -3,12 +3,20 @@ import java.text.DecimalFormat;
 public class WinPercentage{
   public static void main(String[] args){
     Scanner scan = new Scanner(System.in);
-    DecimalFormat fmt = new DecimalFormat(".00");
+    DecimalFormat fmt = new DecimalFormat("0.0");
     System.out.println("# of games played");
     int totalGames = scan.nextInt();
     System.out.println("# of games won");
     int totalWins = scan.nextInt();
-    double winPercentage = totalWins*100.0/totalGames;
-    System.out.println(fmt.format(winPercentage)+"%");
+    if (totalGames<1)
+      System.out.println("# of games played must be greater than zero");
+    else if (totalWins<0)
+      System.out.println("# of games won must be greater than or equal to zero");
+    else if (totalGames<totalWins)
+      System.out.println("# of games won must be less than # of games played");
+    else {
+      double winPercentage = totalWins*100.0/totalGames;
+      System.out.println(fmt.format(winPercentage)+"%");
+    }
   }
 }
