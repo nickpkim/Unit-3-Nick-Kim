41f48b82389f10271a811d6b4bfbae257e414768
Nick Kim <kimn@wlhs.wlwv.k12.or.us>
2019-10-10 08:57:56 -0700

    git ignore and win percent

--- WinPercentage.java
+++ WinPercentage.java
@@ -4,15 +4,16 @@ public class WinPercentage{
   public static void main(String[] args){
     Scanner scan = new Scanner(System.in);
     DecimalFormat fmt = new DecimalFormat("0.0");
-    int totalGames = -69;
-    int totalWins = -69;
-    String throwAway = new String();
+    //int totalGames = -69;
+    //int totalWins = -69;
+  //  String throwAway = new String();
+    /*
     System.out.println("# of games played");
     while (totalGames == -69){
-      if (scan.nextInt()>=1 && !scan.hasNextInt()){
-          totalWins = scan.nextInt();
+      if (scan.nextInt()>=1 && scan.hasNextInt()){
+          totalGames = scan.nextInt();
       } else{
-        //throwAway = scan.next();
+        throwAway = scan.next();
         System.out.println("error: must be a non-decimal integer");
       }
     }
@@ -25,6 +26,34 @@ public class WinPercentage{
         System.out.println("error: must be a non-decimal integer");
       }
     }
+    */
+    double totalGames = 0;
+    boolean valid = false;
+    System.out.println("# of games played");
+    while (valid == false){
+      totalGames = scan.nextDouble();
+      if (totalGames%1 != 0){
+        System.out.println("error: must be a non-decimal integer");
+      }  else if (totalGames<1){
+        System.out.println("error: # of games played must be greater than zero");
+      } else{
+        valid = true;
+      }
+    }
+    double totalWins = 0;
+    valid = false;
+    System.out.println("# of games won");
+    while (valid == false){
+      totalWins = scan.nextDouble();
+      if (totalWins%1 != 0){
+        System.out.println("error: must be a non-decimal integer");
+      } else if (totalWins<0){
+        System.out.println("error: # of games played must be greater than or equal to zero");
+      } else{
+        valid = true;
+      }
+    }
+
     while ((totalGames<1) || (totalWins<0) || (totalWins>totalGames)){
       if (totalGames<1)
         System.out.println("# of games played must be greater than zero");
