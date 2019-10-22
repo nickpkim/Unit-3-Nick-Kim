509055a8b468fd2c200086cd197f6ec0aaad0b62
Nick Kim <kimn@wlhs.wlwv.k12.or.us>
2019-10-22 09:31:40 -0700

    mini project boi

--- MiniProject.java
+++ MiniProject.java
@@ -2,58 +2,69 @@ import java.util.Scanner;
 public class MiniProject{
   public static void main(String[] args){
     Scanner scan = new Scanner(System.in);
-    System.out.println("Welcome to Mega Star Printer X3000");
-    System.out.println("Would you like to print some thingies?");
-    String answer = scan.nextLine().toLowerCase();
-    int length = answer.length();
-    for (int count=0; count<length; count++){
-      if (answer.codePointAt(count)<97 || answer.codePointAt(count)>122){
-        String toRemove = answer.substring(count,count+1);
-        answer = answer.replace(toRemove,"");
-        count = 0;
-        length = answer.length();
-      }
-    }
-    boolean play = false;
-    if (answer.equals("yes") || answer.equals("yeah")){
-      play = true;
-      System.out.println("ok!");
-    }
-    boolean integerValid = false;
-    String integerInput = "";
-    while (integerValid == false){
-      System.out.println("1 2 3 4 or 5");
-      integerInput = scan.nextLine();
-      for (int i=0; i<integerInput.length(); i++){
-        if (integerInput.codePointAt(i)<49 || integerInput.codePointAt(i)>53){
-          System.out.println("error: must be an integer 1-5");
-          break;
-        } else if(i == integerInput.length()-1){
-          integerValid = true;
+    write("Welcome to Mega Star Printer X3000");
+    boolean play = true;
+    boolean multiPrint = false;
+    int multiplePrint = 0;
+    while (play == true){
+      /*System.out.println("Would you like to print some thingies?");
+      String answer = scan.nextLine().toLowerCase();
+      int length = answer.length();
+      for (int count=0; count<length; count++){
+        if (answer.codePointAt(count)<97 || answer.codePointAt(count)>122){
+          String toRemove = answer.substring(count,count+1);
+          answer = answer.replace(toRemove,"");
+          count = 0;
+          length = answer.length();
         }
       }
+      if (answer.equals("yes") || answer.equals("yeah")){
+        System.out.println("ok!");
+      } else{
+        System.out.println("aight");
+        break;
+      }*/
+      boolean integerValid = false;
+      String integerInput = "";
+      while (integerValid == false){
+        write("1 2 3 4 or 5. To quit, enter \"quit\"");
+        integerInput = scan.nextLine();
+        if (integerInput.toLowerCase().equals("quit")){
+          play = false;
+          break;
+        }
+        for (int i=0; i<integerInput.length(); i++){
+          if (integerInput.codePointAt(i)<49 || integerInput.codePointAt(i)>53){
+            write("error: must be an integer 1-5. To quit, enter \"quit\"");
+            break;
+          } else if(i == integerInput.length()-1){
+            integerValid = true;
+          }
+        }
+      }
+      if (play == false){
+        break;
+      }
+      for (int i = 0; i < integerInput.length(); i++){
+        if (integerInput.charAt(i) == '1'){
+          print1();
+        } else if (integerInput.charAt(i) == '2'){
+          print2();
+        } else if (integerInput.charAt(i) == '3'){
+          print3();
+        } else if (integerInput.charAt(i) == '4'){
+          print4();
+        } else if (integerInput.charAt(i) == '5'){
+          print5();
+        }
+      }
+      write("");
     }
-    int integer = Integer.parseInt(integerInput);
-    if (integer == 1){
-      print1();
-    }
-    if (integer == 2){
-      print2();
-    }
-    if (integer == 3){
-      print3();
-    }
-    if (integer == 4){
-      printIsoUp();
-      printIsoDown();
-    }
-    if (integer == 5){
-      printIsoDown();
-      printIsoUp();
-    }
+    write("");
+    write("Goodbye! *bedoop*");
   }
   static void print1(){
-    for (int i = 10; i > 0; i--){
+    for (int i = 9; i > 0; i--){
       System.out.println();
       for (int j = i; j > 0; j--){
         System.out.print("*");
@@ -61,21 +72,21 @@ public class MiniProject{
     }
   }
   static void print2(){
-    for (int i = 0; i < 10; i++){
+    for (int i = 0; i < 9; i++){
       System.out.println();
-      int spaces = 9 - i;
+      int spaces = 8 - i;
       for (int j = 0; j < spaces; j++){
         System.out.print(" ");
       }
-      for (int k = 0; k < 10 - spaces; k++){
+      for (int k = 0; k < 9 - spaces; k++){
         System.out.print("*");
       }
     }
   }
   static void print3(){
-    for (int i = 10; i > 0; i--){
+    for (int i = 9; i > 0; i--){
       System.out.println();
-      int spaces = 10 - i;
+      int spaces = 9 - i;
       for (int j = 0; j < spaces; j++){
         System.out.print(" ");
       }
@@ -115,5 +126,16 @@ public class MiniProject{
         System.out.print(" ");
       }
     }
+  }
+  static void print4(){
+    printIsoUp();
+    printIsoDown();
+  }
+  static void print5(){
+    printIsoDown();
+    printIsoUp();
+  }
+  static void write(String str){
+    System.out.println(str);
   }
 }
